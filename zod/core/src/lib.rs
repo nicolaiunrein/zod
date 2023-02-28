e48d3f2ce98ce7e68d216f677716373c251baf2e
Nicolai Unrein <n.unrein@gmail.com>
2023-02-28 18:04:22 +0100

    move NsMember to zod-core

--- zod/core/src/lib.rs
+++ zod/core/src/lib.rs
@@ -20,12 +20,39 @@ pub trait Namespace {
         None
     }
 
-    // fn code() -> String {
-    // let members =
-    // || inventory::iter::<NsMember>().filter(|member| member.ns_name() == Self::NAME);
+    #[cfg(feature = "inventory")]
+    fn members() -> Vec<&'static NamespaceMemberDefinition> {
+        let members = inventory_crate::iter::<NamespaceMemberDefinition>()
+            .filter(|namespace| namespace.ns_name == Self::NAME);
 
-    // let member_code = members().map(|member| member.decl()).collect::<String>();
-
-    // format!("export namespace {} {{\n{member_code}}};", Self::NAME)
-    // }
+        members.collect()
+    }
 }
+
+type RuntimeValue<T> = &'static (dyn Fn() -> T + Sync);
+
+pub struct NamespaceMemberDefinition {
+    ns_name: &'static str,
+    name: &'static str,
+    schema: RuntimeValue<String>,
+    type_def: RuntimeValue<String>,
+}
+
+impl NamespaceMemberDefinition {
+    pub fn namespace(&self) -> &'static str {
+        self.ns_name
+    }
+    pub fn name(&self) -> &'static str {
+        self.name
+    }
+
+    pub fn schema(&self) -> String {
+        (self.schema)()
+    }
+
+    pub fn type_def(&self) -> String {
+        (self.type_def)()
+    }
+}
+
+inventory_crate::collect!(NamespaceMemberDefinition);
