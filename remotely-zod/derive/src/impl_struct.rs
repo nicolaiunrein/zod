24967bc3995bddec06452cea25c5ad1c03da3aee
Nicolai Unrein <n.unrein@gmail.com>
2023-02-28 13:25:30 +0100

    refactor struct_impl

--- remotely-zod/derive/src/impl_struct.rs
+++ remotely-zod/derive/src/impl_struct.rs
@@ -4,8 +4,8 @@ use super::args;
 use darling::ast::{Fields, Style};
 use proc_macro2::TokenStream;
 use quote::{quote, quote_spanned};
-use serde_derive_internals::ast::{self, Field};
-use syn::spanned::Spanned;
+use serde_derive_internals::ast;
+use syn::{spanned::Spanned, Ident, Path};
 
 fn qualified_ty(ty: &syn::Type) -> proc_macro2::TokenStream {
     quote!(<#ty as ::remotely_zod::Codegen>)
@@ -28,254 +28,244 @@ pub fn expand(
         ast::Data::Struct(_, fields) => fields,
     };
 
-    let schema = expand_schema(transparent, &fields, &fields_ast);
-    let type_def = expand_type_def(transparent, &fields, &fields_ast);
+    let style = fields.style;
 
-    quote! {
-        impl remotely_zod::Codegen for #ident {
-            fn schema() -> String {
-                #schema
-            }
+    let fields = fields
+        .iter()
+        .zip(fields_ast.iter().map(|f| &f.attrs))
+        .filter(|(_, attrs)| !attrs.skip_deserializing())
+        .map(|(args::StructField { ty, ident }, attrs)| StructField {
+            ty: &ty,
+            name: ident.as_ref().map(|_| attrs.name().deserialize_name()),
+            optional: !attrs.default().is_none(),
+            transparent,
+            flatten: attrs.flatten(),
+        })
+        .collect();
 
-            fn type_def() -> String {
-                #type_def
-            }
+    let struct_def = Struct {
+        transparent,
+        ns_path,
+        name,
+        docs,
+        ident,
+        fields,
+        style,
+    };
 
-            fn type_name() -> String {
-                format!("{}.{}", <#ns_path as remotely::__private::codegen::namespace::Namespace>::NAME, #name)
-            }
+    struct_def.expand()
+}
 
-            fn docs() -> Option<&'static str> {
-                Some(#docs)
+struct Struct<'a> {
+    transparent: bool,
+    ident: Ident,
+    ns_path: Path,
+    docs: RustDocs,
+    name: String,
+    fields: Vec<StructField<'a>>,
+    style: Style,
+}
+
+impl<'a> Struct<'a> {
+    fn expand(&self) -> TokenStream {
+        let schema = self.expand_schema();
+        let type_def = self.expand_type_def();
+        let ident = &self.ident;
+        let ns_path = &self.ns_path;
+        let name = &self.name;
+        let docs = &self.docs;
+
+        quote! {
+            impl remotely_zod::Codegen for #ident {
+                fn schema() -> String {
+                    #schema
+                }
+
+                fn type_def() -> String {
+                    #type_def
+                }
+
+                fn type_name() -> String {
+                    format!("{}.{}", <#ns_path as remotely::__private::codegen::namespace::Namespace>::NAME, #name)
+                }
+
+                fn docs() -> Option<&'static str> {
+                    Some(#docs)
+                }
             }
         }
     }
-}
 
-fn expand_type_def(
-    transparent: bool,
-    fields: &Fields<args::StructField>,
-    fields_ast: &[Field],
-) -> TokenStream {
-    let field_type_defs = expand_type_defs(transparent, &fields, &fields_ast);
+    fn expand_type_def(&self) -> TokenStream {
+        let (flattened_field_type_defs, field_type_defs) =
+            self.fields.iter().partition::<Vec<_>, _>(|f| f.flatten);
 
-    let flattened_field_type_defs =
-        expand_flattened_field_type_defs(transparent, &fields, &fields_ast);
-
-    if transparent {
-        field_type_defs
-            .into_iter()
-            .next()
-            .expect("At least one field")
-    } else {
-        match fields.style {
-            Style::Tuple => field_type_defs
+        if self.transparent {
+            field_type_defs
                 .into_iter()
                 .next()
-                .or_else(|| flattened_field_type_defs.into_iter().next())
-                .expect("Newtype"),
-            Style::Struct => {
-                quote! {
-                    let fields: Vec<String> = vec![#(#field_type_defs),*];
-                    let extensions: Vec<String> = vec![#(#flattened_field_type_defs),*];
-                    format!("{{{}}}{}", fields.join(",\n"), extensions.join(""))
-                }
-            }
+                .expect("At least one field")
+                .expand_type_defs()
+        } else {
+            match self.style {
+                Style::Tuple => field_type_defs
+                    .into_iter()
+                    .next()
+                    .or_else(|| flattened_field_type_defs.into_iter().next())
+                    .expect("Newtype")
+                    .expand_type_defs(),
 
-            Style::Unit => unreachable!(),
+                Style::Struct => {
+                    let field_type_defs = field_type_defs.into_iter().map(|f| f.expand_type_defs());
+                    let flattened_field_type_defs = flattened_field_type_defs
+                        .into_iter()
+                        .map(|f| f.expand_type_defs());
+
+                    quote! {
+                        let fields: Vec<String> = vec![#(#field_type_defs),*];
+                        let extensions: Vec<String> = vec![#(#flattened_field_type_defs),*];
+                        format!("{{{}}}{}", fields.join(",\n"), extensions.join(""))
+                    }
+                }
+
+                Style::Unit => unreachable!(),
+            }
         }
     }
-}
 
-fn expand_schema(
-    transparent: bool,
-    fields: &Fields<args::StructField>,
-    fields_ast: &[Field],
-) -> TokenStream {
-    let field_schemas = expand_schemas(transparent, &fields, &fields_ast);
-    let flattened_field_schemas =
-        expand_flattened_fields_schemas(transparent, &fields, &fields_ast);
-    if transparent {
-        field_schemas
-            .into_iter()
-            .next()
-            .expect("At least one field")
-    } else {
-        match fields.style {
-            Style::Tuple => field_schemas
+    fn expand_schema(&self) -> TokenStream {
+        let (flattened_field_schemas, field_schemas): (Vec<_>, Vec<_>) =
+            self.fields.iter().partition(|f| f.flatten);
+
+        if self.transparent {
+            field_schemas
                 .into_iter()
                 .next()
-                .or_else(|| flattened_field_schemas.into_iter().next())
-                .expect("Newtype"),
-            Style::Struct => {
-                quote! {
-                    let fields: Vec<String> = vec![#(#field_schemas),*];
-                    let extensions: Vec<String> = vec![#(#flattened_field_schemas),*];
-                    format!("z.object({{{}}}){}", fields.join(",\n"), extensions.join(""))
-                }
-            }
+                .expect("At least one field")
+                .expand_schema()
+        } else {
+            match self.style {
+                Style::Tuple => field_schemas
+                    .into_iter()
+                    .next()
+                    .or_else(|| flattened_field_schemas.into_iter().next())
+                    .expect("Newtype")
+                    .expand_schema(),
 
-            Style::Unit => unreachable!(),
+                Style::Struct => {
+                    let field_schemas = field_schemas.into_iter().map(|f| f.expand_schema());
+
+                    let flattened_field_schemas = flattened_field_schemas
+                        .into_iter()
+                        .map(|f| f.expand_schema());
+
+                    quote! {
+                        let fields: Vec<String> = vec![#(#field_schemas),*];
+                        let extensions: Vec<String> = vec![#(#flattened_field_schemas),*];
+                        format!("z.object({{{}}}){}", fields.join(",\n"), extensions.join(""))
+                    }
+                }
+
+                Style::Unit => unreachable!(),
+            }
         }
     }
 }
 
-fn expand_schemas(
-    is_transparent: bool,
-    fields: &Fields<args::StructField>,
-    fields_ast: &[ast::Field<'_>],
-) -> Vec<TokenStream> {
-    fields
-        .iter()
-        .zip(fields_ast.iter().map(|f| &f.attrs))
-        .filter(|(_, attrs)| !attrs.skip_deserializing())
-        .filter(|(_, attrs)| !attrs.flatten())
-        .map(|(args::StructField { ty, ident }, attrs)| {
-            let name = attrs.name().deserialize_name();
-
-            let maybe_optional = if !attrs.default().is_none() {
-                quote!(".optional()")
-            } else {
-                quote!("")
-            };
-
-            let ty = qualified_ty(ty);
-
-            match (ident, is_transparent) {
-                (Some(_), false) => {
-                    quote_spanned! {ty.span() =>  format!("{}: {}{},", #name, #ty::schema(), #maybe_optional) }
-                }
-                (Some(_), true) => {
-                    quote_spanned! {ty.span() =>  format!("{}{}", #ty::schema(), #maybe_optional) }
-                }
-                (None, _) => {
-                    quote_spanned! { ty.span() => format!("{}{}", #ty::schema(), #maybe_optional) }
-                }
-            }
-
-        })
-        .collect()
+struct StructField<'a> {
+    name: Option<String>,
+    ty: &'a syn::Type,
+    optional: bool,
+    transparent: bool,
+    flatten: bool,
 }
 
-fn expand_flattened_fields_schemas(
-    is_transparent: bool,
-    fields: &Fields<args::StructField>,
-    fields_ast: &[ast::Field<'_>],
-) -> Vec<TokenStream> {
-    fields
-        .iter()
-        .zip(fields_ast.iter().map(|f| &f.attrs))
-        .filter(|(_, attrs)| !attrs.skip_deserializing())
-        .filter(|(_, attrs)| attrs.flatten())
-        .map(|(args::StructField { ty, ident }, attrs)| {
-            let maybe_optional = if !attrs.default().is_none() {
-                quote!(".optional()")
-            } else {
-                quote!("")
-            };
+impl<'a> StructField<'a> {
+    fn expand_schema(&self) -> TokenStream {
+        let maybe_optional = self.expand_optional_schema();
+        let ty = qualified_ty(&self.ty);
 
-            let ty = qualified_ty(ty);
-
-            match (ident, is_transparent) {
-                (Some(_), false) => {
-                    quote_spanned! {ty.span() =>  format!(".extend({}{})", #ty::schema(), #maybe_optional) }
-                }
-                (Some(_), true) => {
-                    quote_spanned! {ty.span() =>  format!(".extend({}{})", #ty::schema(), #maybe_optional) }
-                }
-                (None, _) => {
-                    // Newtype
-                    quote_spanned! { ty.span() => format!(".extend({}{})", #ty::schema(), #maybe_optional) }
-                }
-
+        match (self.flatten, &self.name, self.transparent) {
+            (false, Some(name), false) => {
+                quote_spanned! {ty.span() =>  format!("{}: {}{},", #name, #ty::schema(), #maybe_optional) }
             }
-        })
-        .collect()
-}
-
-fn expand_type_defs(
-    is_transparent: bool,
-    fields: &Fields<args::StructField>,
-    fields_ast: &[ast::Field<'_>],
-) -> Vec<TokenStream> {
-    fields
-        .iter()
-        .zip(fields_ast.iter().map(|f| &f.attrs))
-        .filter(|(_, attrs)| !attrs.skip_deserializing())
-        .filter(|(_, attrs)| !attrs.flatten())
-        .map(|(args::StructField { ident, ty, .. }, attrs)| {
-            let name = attrs.name().deserialize_name();
-            let is_optional = !attrs.default().is_none();
-            let ty = qualified_ty(ty);
-
-            match (ident, is_optional, is_transparent) {
-                (Some(_), false, false) => {
-                    quote_spanned! {ty.span() =>  format!("{}: {}", #name, #ty::type_name()) }
-                }
-                (None, false, false) => {
-                    // Newtype
-                    quote_spanned! {ty.span() => #ty::type_name()}
-                }
-                (Some(_), true, false) => {
-                    quote_spanned! {ty.span() =>  format!("{}?: {} | undefined", #name, #ty::type_name()) }
-                }
-                (None, true, false) => {
-                    // Newtype
-                    quote_spanned! {ty.span() => format!("{} | undefined", #ty::type_name())}
-                }
-
-
-                (_, false, true) => {
-                    // Newtype
-                    quote_spanned! {ty.span() => #ty::type_name()}
-                }
-                (_, true, true) => {
-                    quote_spanned! {ty.span() =>  format!("{} | undefined", #ty::type_name()) }
-                }
+            (false, Some(_), true) => {
+                quote_spanned! {ty.span() =>  format!("{}{}", #ty::schema(), #maybe_optional) }
             }
-        })
-        .collect()
-}
-
-fn expand_flattened_field_type_defs(
-    is_transparent: bool,
-    fields: &Fields<args::StructField>,
-    fields_ast: &[ast::Field<'_>],
-) -> Vec<TokenStream> {
-    fields
-        .iter()
-        .zip(fields_ast.iter().map(|f| &f.attrs))
-        .filter(|(_, attrs)| !attrs.skip_deserializing())
-        .filter(|(_, attrs)| attrs.flatten())
-        .map(|(args::StructField { ident, ty, .. }, attrs)| {
-            let is_optional = !attrs.default().is_none();
-
-            let ty = qualified_ty(ty);
-
-            match (ident, is_optional, is_transparent) {
-                (Some(_), false, false) => {
-                    quote_spanned! {ty.span() =>  format!(" & {}", #ty::type_name()) }
-                }
-                (None, false, false) => {
-                    // Newtype
-                    quote_spanned! {ty.span() => #ty::type_name()}
-                }
-                (Some(_), true, false) => {
-                    quote_spanned! {ty.span() =>  format!("& ({} | undefined)", #ty::type_name()) }
-                }
-                (None, true, false) => {
-                    // Newtype
-                    quote_spanned! {ty.span() => format!("& ({} | undefined)", #ty::type_name())}
-                }
-
-                (_, false, true) => {
-                    // Newtype
-                    quote_spanned! {ty.span() => #ty::type_name()}
-                }
-                (_, true, true) => {
-                    quote_spanned! {ty.span() =>  format!("& ({} | undefined)", #ty::type_name()) }
-                }
+            (false, None, _) => {
+                quote_spanned! { ty.span() => format!("{}{}", #ty::schema(), #maybe_optional) }
             }
-        })
-        .collect()
+
+            (true, Some(_), false) => {
+                quote_spanned! {ty.span() =>  format!(".extend({}{})", #ty::schema(), #maybe_optional) }
+            }
+            (true, Some(_), true) => {
+                quote_spanned! {ty.span() =>  format!(".extend({}{})", #ty::schema(), #maybe_optional) }
+            }
+            (true, None, _) => {
+                // Newtype
+                quote_spanned! { ty.span() => format!(".extend({}{})", #ty::schema(), #maybe_optional) }
+            }
+        }
+    }
+
+    fn expand_optional_schema(&self) -> TokenStream {
+        if self.optional {
+            quote!(".optional()")
+        } else {
+            quote!("")
+        }
+    }
+
+    fn expand_type_defs(&self) -> TokenStream {
+        let ty = qualified_ty(&self.ty);
+
+        match (self.flatten, &self.name, self.optional, self.transparent) {
+            (false, Some(name), false, false) => {
+                quote_spanned! {ty.span() =>  format!("{}: {}", #name, #ty::type_name()) }
+            }
+            (false, None, false, false) => {
+                // Newtype
+                quote_spanned! {ty.span() => #ty::type_name()}
+            }
+            (false, Some(name), true, false) => {
+                quote_spanned! {ty.span() =>  format!("{}?: {} | undefined", #name, #ty::type_name()) }
+            }
+            (false, None, true, false) => {
+                // Newtype
+                quote_spanned! {ty.span() => format!("{} | undefined", #ty::type_name())}
+            }
+
+            (false, _, false, true) => {
+                // Newtype
+                quote_spanned! {ty.span() => #ty::type_name()}
+            }
+            (false, _, true, true) => {
+                quote_spanned! {ty.span() =>  format!("{} | undefined", #ty::type_name()) }
+            }
+
+            (true, Some(_), false, false) => {
+                quote_spanned! {ty.span() =>  format!(" & {}", #ty::type_name()) }
+            }
+            (true, None, false, false) => {
+                // Newtype
+                quote_spanned! {ty.span() => #ty::type_name()}
+            }
+            (true, Some(_), true, false) => {
+                quote_spanned! {ty.span() =>  format!("& ({} | undefined)", #ty::type_name()) }
+            }
+            (true, None, true, false) => {
+                // Newtype
+                quote_spanned! {ty.span() => format!("& ({} | undefined)", #ty::type_name())}
+            }
+
+            (true, _, false, true) => {
+                // Newtype
+                quote_spanned! {ty.span() => #ty::type_name()}
+            }
+            (true, _, true, true) => {
+                quote_spanned! {ty.span() =>  format!("& ({} | undefined)", #ty::type_name()) }
+            }
+        }
+    }
 }
