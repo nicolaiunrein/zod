0079228d6f33a68bae9f0704a3f7440ee49f62aa
Nicolai Unrein <n.unrein@gmail.com>
2023-05-02 17:28:20 +0200

    improve example

--- examples/axum/web/src/lib/Counter.svelte
+++ examples/axum/web/src/lib/Counter.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
  import {Watchout, Pixera} from "../api";
+ import {onMount} from "svelte";
 
   let count: number = 0
-  const increment = () => {
-    count += 1
+  const increment = async () => {
+    count = await Watchout.hello("abc", 123n);
   }
 
   const x = Watchout.hello_stream(20n);
   const y = Pixera.hello_stream(20n);
 
+  onMount(async () => {
+    count = await Watchout.hello("abc", 123n);
+  });
+
 </script>
 
 <button on:click={increment}>
   count is {count}
-  {$x}
-  {$y}
 </button>
+<p>Watchout: {$x}</p>
+<p>Pixera: {$y}</p>
+
