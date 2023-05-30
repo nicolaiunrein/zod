6899cf5dcdcffc074d661cadfb542346e3eb09e4
Nicolai Unrein <n.unrein@gmail.com>
2023-05-30 19:03:27 +0200

    add tuple enum test cases

--- experiments/derive/tests/debug.rs
+++ experiments/derive/tests/debug.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use zod_derive_experiments::Zod;
 
 struct Ns;
--- experiments/derive/tests/enum.rs
+++ experiments/derive/tests/enum.rs
@@ -14,6 +14,9 @@ impl zod_core::Namespace for Ns {
 #[zod(namespace = "Ns")]
 enum ExternallyTagged {
     Unit,
+    Tuple0(),
+    Tuple1(String),
+    Tuple2(String, u8),
     Struct0 {},
     Struct1 { inner: String },
     Struct2 { inner_string: String, inner_u8: u8 },
@@ -24,6 +27,9 @@ enum ExternallyTagged {
 #[serde(untagged)]
 enum Untagged {
     Unit,
+    Tuple0(),
+    Tuple1(String),
+    Tuple2(String, u8),
     Struct0 {},
     Struct1 { inner: String },
     Struct2 { inner_string: String, inner_u8: u8 },
@@ -44,6 +50,9 @@ enum InternallyTagged {
 #[serde(tag = "my_tag", content = "my_content")]
 enum AdjacentlyTagged {
     Unit,
+    Tuple0(),
+    Tuple1(String),
+    Tuple2(String, u8),
     Struct0 {},
     Struct1 { inner: String },
     Struct2 { inner_string: String, inner_u8: u8 },
@@ -54,6 +63,9 @@ fn externally_tagged_ok() {
     let export: ZodExport<Kind::Input> = ExternallyTagged::export();
     let variants = [
         "z.literal(\"Unit\")",
+        "z.object({ Tuple0: z.tuple([]) })",
+        "z.object({ Tuple1: z.tuple([Rs.input.String]) })",
+        "z.object({ Tuple2: z.tuple([Rs.input.String, Rs.input.U8]) })",
         "z.object({ Struct0: z.object({}) })",
         "z.object({ Struct1: z.object({ inner: Rs.input.String }) })",
         "z.object({ Struct2: z.object({ inner_string: Rs.input.String, inner_u8: Rs.input.U8 }) })",
@@ -73,6 +85,9 @@ fn untagged_tagged_ok() {
     let export: ZodExport<Kind::Input> = Untagged::export();
     let variants = [
         "z.literal(\"Unit\")",
+        "z.tuple([])",
+        "z.tuple([Rs.input.String])",
+        "z.tuple([Rs.input.String, Rs.input.U8])",
         "z.object({})",
         "z.object({ inner: Rs.input.String })",
         "z.object({ inner_string: Rs.input.String, inner_u8: Rs.input.U8 })",
@@ -111,6 +126,9 @@ fn adjacently_tagged_ok() {
     let export: ZodExport<Kind::Input> = AdjacentlyTagged::export();
     let variants = [
         "z.object({ my_tag: z.literal(\"Unit\") })",
+        "z.object({ my_tag: z.literal(\"Tuple0\"), my_content: z.tuple([]) })",
+        "z.object({ my_tag: z.literal(\"Tuple1\"), my_content: z.tuple([Rs.input.String]) })",
+        "z.object({ my_tag: z.literal(\"Tuple2\"), my_content: z.tuple([Rs.input.String, Rs.input.U8]) })",
         "z.object({ my_tag: z.literal(\"Struct0\"), my_content: z.object({}) })",
         "z.object({ my_tag: z.literal(\"Struct1\"), my_content: z.object({ inner: Rs.input.String }) })",
         "z.object({ my_tag: z.literal(\"Struct2\"), my_content: z.object({ inner_string: Rs.input.String, inner_u8: Rs.input.U8 }) })",
